f4e1372ee2053c2d363b9669b12840fa8251c2bb
valkyrie <valkyrie@r3valkyrie.com>
2020-06-18 10:57:31 -0400

    i must do this

--- utils/autotagger/autotagger.py
+++ utils/autotagger/autotagger.py
@@ -1,31 +1,32 @@
-import sys
-import os
-import shutil
-import glob
-import json
+from glob import glob
+from json import dump
+from os import makedirs
+from os.path import abspath
+from shutil import copy
+from sys import exit
 
 
 def main():
-    in_path = os.path.abspath('./in')
-    out_path = os.path.abspath('./out')
-    for pack in glob.glob(f'{in_path}/*'):
+    in_path = abspath('./in')
+    out_path = abspath('./out')
+    for pack in glob(f'{in_path}/*'):
         dirs = [
             f'{out_path}/{pack.split("/")[-1]}/data',
             f'{out_path}/{pack.split("/")[-1]}/textures/objects'
         ]
         for x in dirs:
-            os.makedirs(x, exist_ok=True)
+            makedirs(x, exist_ok=True)
         default = {'tags': {}, 'sets': {}}
-        for tag in [x.split('/')[-1] for x in glob.glob(f'{pack}/textures/objects/tags/*')]:
+        for tag in [x.split('/')[-1] for x in glob(f'{pack}/textures/objects/tags/*')]:
             default['tags'].update({tag: [
-                f'textures/objects/{x.split("/")[-1]}' for x in glob.glob(f'{pack}/textures/objects/tags/{tag}/*')
+                f'textures/objects/{x.split("/")[-1]}' for x in glob(f'{pack}/textures/objects/tags/{tag}/*')
             ]})
-        for img in glob.glob(f'{pack}/textures/objects/**/*.png', recursive=True):
-            shutil.copy(img, dirs[1])
+        for img in glob(f'{pack}/textures/objects/**/*.png', recursive=True):
+            copy(img, dirs[1])
         with open(f'{dirs[0]}/default.dungeondraft_tags', 'w+') as tagfile:
-            json.dump(default, tagfile, sort_keys=False, indent=4)
+            dump(default, tagfile, sort_keys=False, indent=4)
     return 0
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    exit(main())
